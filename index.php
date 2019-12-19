22844bd5680d0c162277d16a0e14c9bfe7d0aac7
mehedi101 <mehedi101@outlook.com>
2019-12-19 21:37:29 +0600

    Blog and Blog single post page done

--- index.php
+++ index.php
@@ -1,11 +1,45 @@
 <?php get_header(); ?>
+
+
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('images/library-hero.jpg') ?>);"></div>
-        <div class="page-banner__content container t-center c-white">
-            <h1 class="headline headline--large">Welcome!</h1>
-            <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-            <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-            <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?= get_theme_file_uri('images/ocean.jpg') ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">Welcome to our blog </h1>
+            <div class="page-banner__intro">
+                <p> Keep up with our latest news!</p>
+            </div>
         </div>
     </div>
+
+
+    <div class="container container--narrow page-section">
+        <?php while (have_posts()): the_post() ?>
+            <div class="post-item">
+                <h2 class="headline headline--medium headline--post-title">
+                    <a href=" <?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h2>
+
+                <div class="metabox">
+                    <span class="post--author"> Posted by <?php  the_author_posts_link() ; ?></span>
+                    <span class="post--date"> on <?php the_time('d.m.y')  ; ?> </span>
+                    <span class="post--category"> in  <?php echo get_the_category_list(', ')  ; ?></span>
+                </div>
+                <div class="generic-content">
+                    <?php
+                    the_excerpt();
+
+                    ?>
+                    <p><a class="btn btn--blue" href="<?php the_permalink(); ?>"> Continue Reading &raquo;</a></p>
+                </div>
+
+            </div>
+
+        <?php endwhile; ?>
+
+       <div class="post--pagination"> <?php echo paginate_links()  ; ?></div>
+
+
+    </div>
+
 <?php get_footer(); ?>
