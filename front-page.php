1e214ad071ab1364effea10dbecddd45225c8763
mehedi101 <mehedi101@outlook.com>
2020-02-25 22:35:21 +0600

    working with events

--- front-page.php
+++ front-page.php
@@ -18,33 +18,48 @@
             <div class="full-width-split__inner">
                 <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-                    <?php
-                                $homepageEvents = new WP_Query([
-                                        'posts_per_page' => 2, 'post_type' => 'event'
-                                ]);
-                                while ($homepageEvents->have_posts()): $homepageEvents->the_post() ?>
-                                    <div class="event-summary">
-                                        <a class="event-summary__date t-center" href="#">
-                                            <span class="event-summary__month"><?php the_time('M') ;?></span>
-                                            <span class="event-summary__day"><?php the_time('d') ;?></span>
-                                        </a>
-                                        <div class="event-summary__content">
-                                            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                                            <p> <?php echo (has_excerpt()) ? get_the_excerpt() : wp_trim_words(get_the_content(), 18);  ?>
-                                                <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
-                                        </div>
-                                    </div>
+                <?php
+                $today = date('Y-m-d');
+                $homepageEvents = new WP_Query([
+                    'posts_per_page' => 2,
+                    'post_type' => 'event',
+                    'meta_query' => [[
+                            'key' => 'event_date',
+                            'compare' => '>=',
+                            'value' => $today,
+                            'type' => 'date'
+                    ]],
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value',
+                    'order' => 'Asc'
+                ]);
+                while ($homepageEvents->have_posts()): $homepageEvents->the_post() ?>
+                    <div class="event-summary">
+                        <a class="event-summary__date t-center" href="#">
+                                            <span class="event-summary__month"><?php
+                                                $eventDate = new DateTime(get_field('event_date'));
 
-                                <?php
-                                endwhile;
+                                                echo $eventDate->format('M') ?></span>
+                            <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
+                        </a>
+                        <div class="event-summary__content">
+                            <h5 class="event-summary__title headline headline--tiny"><a
+                                        href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
+                            <p> <?php echo (has_excerpt()) ? get_the_excerpt() : wp_trim_words(get_the_content(),
+                                    18); ?>
+                                <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a></p>
+                        </div>
+                    </div>
 
-                                wp_reset_postdata();
-                                ?>
+                <?php
+                endwhile;
+
+                wp_reset_postdata();
+                ?>
 
 
-
-
-                <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
+                <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>"
+                                                 class="btn btn--blue">View All Events</a></p>
 
             </div>
         </div>
@@ -81,7 +96,8 @@
                             <h5 class="event-summary__title headline headline--tiny">
                                 <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                             </h5>
-                            <p> <?php echo (has_excerpt()) ? get_the_excerpt() : wp_trim_words(get_the_content(), 18);  ?>
+                            <p> <?php echo (has_excerpt()) ? get_the_excerpt() : wp_trim_words(get_the_content(),
+                                    18); ?>
                                 <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a>
                             </p>
                         </div>
@@ -92,7 +108,8 @@
                 ?>
 
 
-                <p class="t-center no-margin"><a href="<?php echo site_url('/blog')  ; ?>" class="btn btn--yellow">View All Blog Posts</a></p>
+                <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View
+                        All Blog Posts</a></p>
             </div>
         </div>
     </div>
